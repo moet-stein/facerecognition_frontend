8b0ca5d7ca5ca44427a599af74eb4223ad18fce5
moe-stein <0101.mste@gmail.com>
2020-07-22 19:12:35 +0900

    Updates

---
+++ precache-manifest.d24ae495d89d068468ae9d09a8de4c17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6709109a5a125847cb78d48d298172e7",
+    "url": "/facerecognition_frontend/index.html"
+  },
+  {
+    "revision": "b167b5a5e90ae613090d",
+    "url": "/facerecognition_frontend/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b1080d9735e602f221ea",
+    "url": "/facerecognition_frontend/static/css/main.74a019f8.chunk.css"
+  },
+  {
+    "revision": "b167b5a5e90ae613090d",
+    "url": "/facerecognition_frontend/static/js/2.027475f2.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facerecognition_frontend/static/js/2.027475f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1080d9735e602f221ea",
+    "url": "/facerecognition_frontend/static/js/main.6615f97e.chunk.js"
+  },
+  {
+    "revision": "5345387ad0820a5ad346",
+    "url": "/facerecognition_frontend/static/js/runtime-main.f234fbd8.js"
+  }
+]);
